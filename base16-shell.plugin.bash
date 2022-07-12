415b5974fe14eaf224a83b957406b70d823f1639
Josh Bode <joshbode@fastmail.com>
2022-07-13 07:59:39 +1000

    Switch return to exit to prevent shell errors

--- base16-shell.plugin.bash
+++ base16-shell.plugin.bash
@@ -2,5 +2,5 @@ script_path=${BASH_SOURCE[0]}
 script_path=$(readlink -f $script_path)
 BASE16_SHELL_PATH=${script_path%/*}
 
-[ -n "$PS1" ] && [ -s $BASE16_SHELL_PATH/profile_helper.sh ] && source \
+[ -n "$PS1" ] && [ -s $BASE16_SHELL_PATH/profile_helper.sh ] && \
   "$BASE16_SHELL_PATH/profile_helper.sh"
