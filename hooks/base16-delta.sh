ca3be0f6c607396e6898fd55c121e43be40d9a94
Bez Hermoso <bezalelhermoso@gmail.com>
2023-08-21 22:30:39 -0700

    chore: Remove debug statements

--- hooks/base16-delta.sh
+++ hooks/base16-delta.sh
@@ -6,8 +6,6 @@
 
 DELTA_GITCONFIG_PATH="$BASE16_CONFIG_PATH/delta.gitconfig"
 
-echo $DELTA_GITCONFIG_PATH
-
 if [ -f "$DELTA_GITCONFIG_PATH" ]; then
   touch "$DELTA_GITCONFIG_PATH"
 fi
@@ -26,13 +24,13 @@ read current_theme_name < "$BASE16_SHELL_THEME_NAME_PATH"
 # Determine if theme is dark or light based on HSP calculation:
 # http://alienryderflex.com/hsp.html
 
-# We'll calculate the "perceived brightness" of the theme's background color:
-current_bg_color=$BASE16_COLOR_00_HEX
+# We'll calculate the "perceived brightness" of the theme's background color.
+# We will use `bc`, and it only understands upper-case hex values:
+current_bg_color=$(echo $BASE16_COLOR_00_HEX | tr '[:lower:]' '[:upper:]')
 
-# Convert the hex colors to decimal using `bc`. We need to convert them to uppercase first:
-r_hex_value=$(echo ${current_bg_color:0:2} | tr '[:lower:]' '[:upper:]')
-g_hex_value=$(echo ${current_bg_color:2:2} | tr '[:lower:]' '[:upper:]')
-b_hex_value=$(echo ${current_bg_color:4:2} | tr '[:lower:]' '[:upper:]')
+r_hex_value=${current_bg_color:0:2}
+g_hex_value=${current_bg_color:2:2}
+b_hex_value=${current_bg_color:4:2}
 
 # Calculate the perceived brightness, and check against brightness threshold of 7F.8 (127.5 in decimal):
 echo "ibase=16; sqrt((.4C8 * ${r_hex_value} ^ 2) + (0.964 * ${g_hex_value} ^ 2) + (.1D2 * ${b_hex_value} ^ 2)) > 7F.8" | bc | read hsp
