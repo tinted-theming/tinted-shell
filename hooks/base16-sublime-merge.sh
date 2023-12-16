98078ba1261b28b1eb36e1c6d219c20f5d4e4c65
Jamy <code@jamygolden.com>
2023-12-16 21:21:31 +0100

    Add SublimeMerge profile_helper hook (#42)

---
+++ hooks/base16-sublime-merge.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------
+# Setup config variables and env
+# ----------------------------------------------------------------------
+
+if [ -z "$BASE16_SHELL_SUBLIMEMERGE_PACKAGE_PATH" ]; then
+  exit 1
+fi
+
+if ! [ -f "$BASE16_SHELL_THEME_NAME_PATH" ]; then
+  exit 1
+fi
+
+# The path/to/sublime-merge/Package must be set
+if ! [ -d "$BASE16_SHELL_SUBLIMEMERGE_PACKAGE_PATH" ]; then
+  exit 1
+fi
+
+# The base16-sublime-merge repo must be cloned at
+if ! [ -d "$BASE16_SHELL_SUBLIMEMERGE_PACKAGE_PATH/base16-sublime-merge" ]; then
+  exit 1
+fi
+
+BASE16_SHELL_SUBLIMEMERGE_SETTINGS_PATH="$BASE16_SHELL_SUBLIMEMERGE_PACKAGE_PATH"/User/Preferences.sublime-settings 
+
+# The Sublime Merge settings path should exist
+if ! [ -f "$BASE16_SHELL_SUBLIMEMERGE_SETTINGS_PATH" ]; then
+  exit 1
+fi
+
+find_replace_json_value_in_sublimemerge_settings() {
+  local property=$1
+  local value=$2
+  local json_file="$BASE16_SHELL_SUBLIMEMERGE_PACKAGE_PATH"/User/Preferences.sublime-settings
+
+  # Use sed to find the property and change its value
+  sed -i "s/\"$property\": \".*\"/\"$property\": \"$value\"/" $BASE16_SHELL_SUBLIMEMERGE_SETTINGS_PATH
+}
+
+# ----------------------------------------------------------------------
+# Execution
+# ----------------------------------------------------------------------
+read current_theme_name < "$BASE16_SHELL_THEME_NAME_PATH"
+
+find_replace_json_value_in_sublimemerge_settings "theme" "base16-$current_theme_name.sublime-theme"
+find_replace_json_value_in_sublimemerge_settings "color_scheme" "base16-$current_theme_name.sublime-color-scheme"
