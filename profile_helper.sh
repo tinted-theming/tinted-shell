3ecbdfc70f99757f994a7e3cb42a7923c9b0cdfc
Jamy <code@jamygolden.com>
2026-02-09 11:02:32 +0100

    Fix bug where env var was accidentally replaced with a string (#68)
    Co-authored-by: Jamy Golden <jamy.eric.golden@m10s.io>

--- profile_helper.sh
+++ profile_helper.sh
@@ -6,7 +6,7 @@
 
 # Allow users to optionally configure where their tinted-shell config is
 # stored by specifying BASE16_CONFIG_PATH before loading this script
-if [ -z "BASE16_CONFIG_PATH" ]; then
+if [ -z "$BASE16_CONFIG_PATH" ]; then
   BASE16_CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/tinted-theming"
 fi
 BASE16_SHELL_COLORSCHEME_PATH="$BASE16_CONFIG_PATH/base16_shell_theme"
